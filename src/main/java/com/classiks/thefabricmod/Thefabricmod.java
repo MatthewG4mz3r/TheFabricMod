e7c2d67c207ac8a955f8d3f93aadd0f16e58a604
mattheww <matthew@Beas-iMac.local>
2022-08-24 18:05:58 +0100

    Initial commit

---
+++ src/main/java/com/classiks/thefabricmod/Thefabricmod.java
@@ -0,0 +1,15 @@
+package com.classiks.thefabricmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Thefabricmod implements ModInitializer {
+    public static final String MODID = "thefabricmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/classiks/thefabricmod/client/ThefabricmodClient.java
@@ -0,0 +1,12 @@
+package com.classiks.thefabricmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+
+public class ThefabricmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
